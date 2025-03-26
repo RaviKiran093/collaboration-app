1bebd83d26c4e1123a95aff4f022f2d737db0080
KALYAMPUDI RAVI KIRAN <kalyampudiravi07@gmail.com>
2025-03-26 23:03:29 +0530

    Serve React build from server

--- server/server.js
+++ server/server.js
@@ -1,38 +1,14 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const path = require("path");
+const express = require("express");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',  // Allow requests from any client
-        methods: ['GET', 'POST']
-    }
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-});
-
-// WebSocket for chat
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('chatMessage', (msg) => {
-        console.log('Message received:', msg);
-        io.emit('chatMessage', msg);  // Send to all clients
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
